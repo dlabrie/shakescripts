b801d4773f16042bd71d3f97b3505ba68a3d47dc
Dominick Labrie <dominicklabrie@mbp.dominick.labrie.ca>
2021-06-18 18:57:59 -0300

    Automatically refresh auth token every 5 minutes

---
+++ stay_signed_in.js
@@ -0,0 +1,25 @@
+const refreshAuthToken = async () => {
+  var AuthResponse = await fetch("https://api.shakepay.com/authentication", {
+      "headers": {
+          "accept": "application/json",
+          "accept-language": "en-US,en;q=0.9,fr;q=0.8",
+          "authorization": window.sessionStorage.getItem("feathers-jwt"),
+          "content-type": "application/json",
+          "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Microsoft Edge\";v=\"90\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site"
+      },
+      "referrerPolicy": "same-origin",
+      "body": "{\"strategy\": \"jwt\",\"authToken\": \"" + window.sessionStorage.getItem("feathers-jwt") + "\"}",
+      "method": "POST",
+      "mode": "cors",
+      "credentials": "include"
+  });
+  var Auth = await AuthResponse.json();
+  console.log(Auth);
+  window.sessionStorage.setItem("feathers-jwt", Auth.accessToken);
+};
+
+setInterval(function () { refreshAuthToken() }, 300000);
